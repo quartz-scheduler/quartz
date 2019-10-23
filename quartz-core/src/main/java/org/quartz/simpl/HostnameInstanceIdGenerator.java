48ee2dfce2141a7e5cc40f8b35d28595f3326bdf
yzsunlight <yzsunlight@qq.com>
2019-10-23 13:43:50 +0800

    modifiy HostnameInstanceIdGenerator.java
    config
    org.quartz.scheduler.instanceId =AUTO
    
    When the host name cannot be obtained, it is recommended to generate one randomly

--- quartz-core/src/main/java/org/quartz/simpl/HostnameInstanceIdGenerator.java
+++ quartz-core/src/main/java/org/quartz/simpl/HostnameInstanceIdGenerator.java
@@ -41,6 +41,7 @@ public class HostnameInstanceIdGenerator implements InstanceIdGenerator {
     public String generateInstanceId() throws SchedulerException {
         try {
             return InetAddress.getLocalHost().getHostName();
+            
         } catch (Exception e) {
             throw new SchedulerException("Couldn't get host name!", e);
         }
