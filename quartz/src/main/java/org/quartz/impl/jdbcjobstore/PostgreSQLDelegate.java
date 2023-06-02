bef204ccfec1a6c707558a6e93ea91a159b99370
Wei Huang <weih@kahoot.com>
2023-06-02 10:56:20 +0200

    Copy selectTriggerToAcquire from StdJDBCDelegate
    Signed-off-by: Wei Huang <weih@kahoot.com>

--- quartz/src/main/java/org/quartz/impl/jdbcjobstore/PostgreSQLDelegate.java
+++ quartz/src/main/java/org/quartz/impl/jdbcjobstore/PostgreSQLDelegate.java
@@ -17,13 +17,21 @@
 
 package org.quartz.impl.jdbcjobstore;
 
+import static org.quartz.TriggerKey.triggerKey;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.util.LinkedList;
+import java.util.List;
+import org.quartz.TriggerKey;
 import org.quartz.spi.ClassLoadHelper;
 import org.slf4j.Logger;
 
@@ -93,6 +101,42 @@ public class PostgreSQLDelegate extends StdJDBCDelegate {
             return binaryInput;
         }
         return getObjectFromBlob(rs, colName);
+    }
+
+    @Override
+    public List<TriggerKey> selectTriggerToAcquire(Connection conn, long noLaterThan, long noEarlierThan, int maxCount)
+        throws SQLException {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<TriggerKey> nextTriggers = new LinkedList<TriggerKey>();
+        try {
+            ps = conn.prepareStatement(rtp(SELECT_NEXT_TRIGGER_TO_ACQUIRE));
+
+            // Set max rows to retrieve
+            if (maxCount < 1)
+                maxCount = 1; // we want at least one trigger back.
+            ps.setMaxRows(maxCount);
+
+            // Try to give jdbc driver a hint to hopefully not pull over more than the few rows we actually need.
+            // Note: in some jdbc drivers, such as MySQL, you must set maxRows before fetchSize, or you get exception!
+            ps.setFetchSize(maxCount);
+
+            ps.setString(1, STATE_WAITING);
+            ps.setBigDecimal(2, new BigDecimal(String.valueOf(noLaterThan)));
+            ps.setBigDecimal(3, new BigDecimal(String.valueOf(noEarlierThan)));
+            rs = ps.executeQuery();
+
+            while (rs.next() && nextTriggers.size() < maxCount) {
+                nextTriggers.add(triggerKey(
+                    rs.getString(COL_TRIGGER_NAME),
+                    rs.getString(COL_TRIGGER_GROUP)));
+            }
+
+            return nextTriggers;
+        } finally {
+            closeResultSet(rs);
+            closeStatement(ps);
+        }
     }
 }
 
