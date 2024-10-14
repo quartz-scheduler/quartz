7fa295ec94372945f38b5e0942bd883515288e5f
Carlos Garcia <bcode@protonmail.com>
2024-10-14 21:25:16 +0200

    style:move unnecessary semi-colon
    Signed-off-by: Carlos Garcia <bcode@protonmail.com>

--- quartz/src/main/java/org/quartz/CronExpression.java
+++ quartz/src/main/java/org/quartz/CronExpression.java
@@ -957,7 +957,6 @@ public final class CronExpression implements Serializable, Cloneable {
 
     protected int skipWhiteSpace(int i, String s) {
         for (; i < s.length() && (s.charAt(i) == ' ' || s.charAt(i) == '\t'); i++) {
-            ;
         }
 
         return i;
@@ -965,7 +964,6 @@ public final class CronExpression implements Serializable, Cloneable {
 
     protected int findNextWhiteSpace(int i, String s) {
         for (; i < s.length() && (s.charAt(i) != ' ' || s.charAt(i) != '\t'); i++) {
-            ;
         }
 
         return i;
