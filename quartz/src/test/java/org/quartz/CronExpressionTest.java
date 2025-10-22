50f79608292ce75bc03340963e76c89b778fdcd2
James House <jhouse@revolition.net>
2025-10-22 15:02:29 -0400

    fix #1283 , poor detection of invalid expressions
    Signed-off-by: James House <jhouse@revolition.net>

--- quartz/src/test/java/org/quartz/CronExpressionTest.java
+++ quartz/src/test/java/org/quartz/CronExpressionTest.java
@@ -478,6 +478,17 @@ public class CronExpressionTest extends SerializationTestSupport {
         }
     }
     
+    public void testIsValidExpression() throws Exception {
+
+        assertTrue(CronExpression.isValidExpression("* * * * * ?"));
+        assertTrue(CronExpression.isValidExpression("0 * 5 * * ?"));
+        assertTrue(CronExpression.isValidExpression("0 15 10 L-1W,L-1 * ? 2010"));
+        assertFalse(CronExpression.isValidExpression("Ralf 30 * * * ?"));
+        assertFalse(CronExpression.isValidExpression("0 30 Ralf * * ?"));
+        assertFalse(CronExpression.isValidExpression("kilroy was here"));
+        assertFalse(CronExpression.isValidExpression("L 30 * * * ?"));
+    }
+    
     // execute with version number to generate a new version's serialized form
     public static void main(String[] args) throws Exception {
         new CronExpressionTest().writeJobDataFile("1.5.2");
